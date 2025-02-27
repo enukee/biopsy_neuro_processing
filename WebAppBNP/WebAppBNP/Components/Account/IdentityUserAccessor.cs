04137bd52602dcebc08c01fa066a8ad27bbc6487
vika <rodina2017g@yandex.ru>
2025-02-27 14:24:12 +0300

    Adding to the project ASP.NET Core Identity

---
+++ WebAppBNP/WebAppBNP/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+﻿using WebAppBNP.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebAppBNP.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<WebAppBNPUser> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<WebAppBNPUser> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}
--- WebAppBNP/WebAppBNP/Program.cs
+++ WebAppBNP/WebAppBNP/Program.cs
@@ -1,7 +1,15 @@
 using WebAppBNP.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebAppBNP.Components.Account;
+using WebAppBNP.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("WebAppBNPContextConnection") ?? throw new InvalidOperationException("Connection string 'WebAppBNPContextConnection' not found.");;
+
+builder.Services.AddDbContext<WebAppBNPContext>(options => options.UseSqlServer(connectionString));
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -9,6 +17,28 @@ builder.Services.AddRazorComponents()
 
 // Register HttpClient service
 builder.Services.AddHttpClient();
+
+builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddScoped<IdentityUserAccessor>();
+
+builder.Services.AddScoped<IdentityRedirectManager>();
+
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddIdentityCookies();
+
+builder.Services.AddIdentityCore<WebAppBNPUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<WebAppBNPContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<WebAppBNPUser>, IdentityNoOpEmailSender>();
 
 var app = builder.Build();
 
@@ -27,5 +57,7 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapAdditionalIdentityEndpoints();;
 
 app.Run();
