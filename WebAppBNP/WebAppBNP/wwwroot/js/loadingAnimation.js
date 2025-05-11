720da737a6b1231bede49b152ab864042411ffde
vika <rodina2017g@yandex.ru>
2025-05-11 18:31:25 +0300

    Added a download line

---
+++ WebAppBNP/WebAppBNP/wwwroot/js/loadingAnimation.js
@@ -0,0 +1,18 @@
+﻿window.loadingAnimation = {
+    start: function (elementId) {
+        const element = document.getElementById(elementId);
+        if (element) {
+            let dots = '';
+            const interval = setInterval(function () {
+                dots = dots.length >= 3 ? '' : dots + '.';
+                element.innerText = `Загрузка${dots}`;
+            }, 500);
+
+            // Возвращаем интервал, чтобы его можно было остановить позже
+            return interval;
+        }
+    },
+    stop: function (intervalId) {
+        clearInterval(intervalId);
+    }
+};
