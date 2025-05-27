7f82c353dadc136feea08abc1d0be91200ecb5f8
vika <rodina2017g@yandex.ru>
2025-05-27 11:05:53 +0300

    creating an image series processing page

---
+++ WebAppBNP/WebAppBNP/wwwroot/js/progress.js
@@ -0,0 +1,10 @@
+﻿function startProgress(progress) {
+    const progressBar = document.getElementById('progressBar');
+    if (progress < 0) {
+        progress = 0;
+    } else if (progress > 100) {
+        progress = 100;
+    }
+    progressBar.style.width = progress + '%';
+    progressBar.textContent = progress + '%';
+}
